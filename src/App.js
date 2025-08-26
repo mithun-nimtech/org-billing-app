6b5453c0b05756df33334471d20ee4f6feaf99a6
Mithun <mithun.nimtech@gmail.com>
2025-08-26 17:00:39 +0530

    Updated Home page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Sales from "./pages/Sales";
 import QuickSale from "./pages/QuickSale"; // Import the new QuickSale page
+import Purchases from "./pages/Purchases";
 import { Box, Toolbar } from "@mui/material";
 
 const drawerWidth = 220;
 const HEADER_HEIGHT = 64;
 const Home = () => <h2>Dashboard Home</h2>;
 const Zra = () => <h2>Zra Page</h2>;
-const Purchases = () => <h2>Purchases Page</h2>;
+// const Purchases = () => <h2>Purchases Page</h2>;
 const Products = () => <h2>Products Page</h2>;
 const Warehouses = () => <h2>Warehouses Page</h2>;
 const Projects = () => <h2>Projects Page</h2>;
